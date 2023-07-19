431e3eaeb8bb43634a6d0762189eba4c69a1e52d
Mehmet Ozder <mehmet.ozder@dataflowx.com>
2023-07-19 09:59:00 +0300

    added ocr, and modiy the config

--- app.py
+++ app.py
@@ -1,9 +1,58 @@
 from flask import Flask, request, render_template
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+import cv2
 import random
 import string
+import toml
 
 app = Flask(__name__)
+
+# Konfigürasyon dosyasını yükle
+config = toml.load('config.toml')
+
+# Anahtarın ayar dosyasından alınması
+KEY = config['APP']['EncryptionKey']
+
+# Yasaklı kelimelerin ayar dosyasından alınması
+forbidden_words = config['APP']['ForbiddenWords']
+
+def encrypt_image(image):
+    encrypted_data = bytearray()
+    key_index = 0
+
+    for byte in image.tobytes():
+        encrypted_byte = byte ^ ord(KEY[key_index])
+        encrypted_data.append(encrypted_byte)
+        key_index = (key_index + 1) % len(KEY)
+
+    return Image.frombytes(image.mode, image.size, bytes(encrypted_data))
+
+def decrypt_image(image):
+    decrypted_data = bytearray()
+    key_index = 0
+
+    for byte in image.tobytes():
+        decrypted_byte = byte ^ ord(KEY[key_index])
+        decrypted_data.append(decrypted_byte)
+        key_index = (key_index + 1) % len(KEY)
+
+    return Image.frombytes(image.mode, image.size, bytes(decrypted_data))
+
+def perform_ocr(image):
+    # Basit OCR işlemi yerine çekirdek OCR kütüphanelerini kullanmak için ilgili kodu ekleyebilirsiniz.
+    pass
+
+def check_forbidden_words(text):
+    for word in forbidden_words:
+        if word.lower() in text.lower():
+            return True
+    return False
+
+def draw_rectangle(image, coordinates):
+    draw = ImageDraw.Draw(image)
+    draw.rectangle(coordinates, outline="red", width=3)
+    del draw
+    return image
 
 @app.route('/')
 def index():
@@ -11,45 +60,33 @@ def index():
 
 @app.route('/encrypt', methods=['POST'])
 def encrypt():
-    key = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
-    image = request.files['image']
-
-    img = Image.open(image)
-    img_data = img.convert("RGB").tobytes()
-
-    encrypted_data = bytearray()
-    key_index = 0
-
-    for byte in img_data:
-        encrypted_byte = byte ^ ord(key[key_index])
-        encrypted_data.append(encrypted_byte)
-        key_index = (key_index + 1) % len(key)
-
-    encrypted_img = Image.frombytes("RGB", img.size, bytes(encrypted_data))
-    encrypted_img.save("static/encrypted_image.png")
-
-    return render_template('result.html', image_path='static/encrypted_image.png', key=key)
+    image_file = request.files['image']
+    image = Image.open(image_file)
+    encrypted_image = encrypt_image(image)
+    encrypted_image.save("static/encrypted_image.png")
+    return render_template('result.html', image_path='static/encrypted_image.png', key=KEY)
 
 @app.route('/decrypt', methods=['POST'])
 def decrypt():
-    key = request.form.get('key')
-    encrypted_image = request.files['encrypted_image']
+    encrypted_image_file = request.files['encrypted_image']
+    encrypted_image = Image.open(encrypted_image_file)
+    decrypted_image = decrypt_image(encrypted_image)
+    decrypted_image.save("static/decrypted_image.png")
 
-    img = Image.open(encrypted_image)
-    img_data = img.convert("RGB").tobytes()
+    ocr_result = ""
+    has_forbidden_words = False
 
-    decrypted_data = bytearray()
-    key_index = 0
+    if has_forbidden_words:
+        image_with_rectangles = draw_rectangle(decrypted_image.copy(), [(0, 0), decrypted_image.size])
 
-    for byte in img_data:
-        decrypted_byte = byte ^ ord(key[key_index])
-        decrypted_data.append(decrypted_byte)
-        key_index = (key_index + 1) % len(key)
+        font = ImageFont.truetype("arial.ttf", 14)
+        draw = ImageDraw.Draw(image_with_rectangles)
+        draw.text((10, 10), ocr_result, fill="red", font=font)
 
-    decrypted_img = Image.frombytes("RGB", img.size, bytes(decrypted_data))
-    decrypted_img.save("static/decrypted_image.png")
+        image_with_rectangles.save("static/decrypted_image_marked.png")
+        return render_template('result.html', image_path='static/decrypted_image_marked.png', key=KEY, ocr_result=ocr_result, has_forbidden_words=has_forbidden_words)
 
-    return render_template('result.html', image_path='static/decrypted_image.png')
+    return render_template('result.html', image_path='static/decrypted_image.png', key=KEY, ocr_result=ocr_result, has_forbidden_words=has_forbidden_words)
 
 if __name__ == '__main__':
     app.run(debug=True)
